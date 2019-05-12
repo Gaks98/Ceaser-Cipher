bfd9a17dd443086c9fb67dd4c2946e06f822c9a0
Gaks98 <yvonnegax98@gmail.com>
2019-05-12 22:47:53 +0300

    used the dot method to call class Caesar in the App file

---
+++ src/main/java/App.java
@@ -0,0 +1,18 @@
+ import java.util.Scanner;
+  public class App{
+  public static void  main(String[] args){
+        Caesar caesar = new Caesar();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter word for encryption ");
+        String word = input.nextLine();
+        System.out.println("Enter shift");
+        int numberShift = input.nextInt();
+        String cipher = caesar.encrypt(word,numberShift);
+        System.out.println(cipher);
+
+         String decrypted = caesar.decrypt(cipher,numberShift);
+         System.out.println(decrypted);
+
+
+    }
+}
