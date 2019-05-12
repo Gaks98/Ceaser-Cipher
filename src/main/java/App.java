fb9a7e32c8117024699e2cc6b1616c11d295ad59
Gaks98 <yvonnegax98@gmail.com>
2019-05-12 21:44:33 +0300

    first test failed

---
+++ src/main/java/App.java
@@ -0,0 +1,12 @@
+ import java.util.Scanner;
+  public class App{
+  public static void  main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter word for encryption ");
+        String word = input.nextLine();
+        System.out.println("Enter shift");
+        int numberShift = input.nextInt();
+
+
+    }
+}
---
+++ src/test/java/CaesarTest.java
@@ -0,0 +1,10 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+public class CaesarTest {
+
+    @Test
+    public static String encrypt(String plainText, int shift) {
+        String cipherText = "";
+        return cipherText;
+    }
+}
