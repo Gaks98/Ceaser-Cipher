8d637c87650cefda30bfa55f46471df5e4007632
Gaks98 <yvonnegax98@gmail.com>
2019-05-21 11:21:39 +0300

    added gitignore file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,17 +1,17 @@
- import java.util.Scanner;
-  public class App{
-  public static void  main(String[] args){
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) {
         Caesar caesar = new Caesar();
         Scanner input = new Scanner(System.in);
         System.out.println("Enter word for encryption ");
         String word = input.nextLine();
         System.out.println("Enter shift");
         int numberShift = input.nextInt();
-        String cipher = caesar.encrypt(word,numberShift);
+        String cipher = caesar.encrypt(word, numberShift);
         System.out.println(cipher);
 
-         String decrypted = caesar.decrypt(cipher,numberShift);
-         System.out.println(decrypted);
+        String decrypted = caesar.decrypt(cipher, numberShift);
+        System.out.println(decrypted);
 
 
     }
--- src/main/java/Caesar.java
+++ src/main/java/Caesar.java
@@ -1,80 +1,72 @@
-public class Caesar{
-public static String encrypt(String plainText, int shift){
-        if (shift > 26){
-        shift = shift%26;
-        }
-        else if(shift<0){
-        shift = (shift%26) + 26;
-        }
-        String cipherText = "";
-        int length = plainText.length();
-        for (int i = 0; i <length; i++){
-        char ch = plainText.charAt(i);
-        if (Character.isLetter(ch)){
-        if (Character.isLowerCase(ch)){
-        char c = (char)(ch + shift);
-        if (c > 'z') {
-        cipherText += (char)(ch - (26 - shift));
-        }
-        else {
-        cipherText +=c;
-        }
-        }
-        if (Character.isUpperCase(ch)){
-        char c = (char)(ch + shift);
-        if (c > 'Z') {
-        cipherText += (char)(ch - (26 - shift));
-        }
-        else {
-        cipherText +=c;
-        }
-        }
-        }
-        else{
-        cipherText += ch;
-        }
+public class Caesar {
+        public static String encrypt(String plainText, int shift) {
+                if (shift > 26) {
+                        shift = shift % 26;
+                } else if (shift < 0) {
+                        shift = (shift % 26) + 26;
+                }
+                String cipherText = "";
+                int length = plainText.length();
+                for (int i = 0; i < length; i++) {
+                        char ch = plainText.charAt(i);
+                        if (Character.isLetter(ch)) {
+                                if (Character.isLowerCase(ch)) {
+                                        char c = (char)(ch + shift);
+                                        if (c > 'z') {
+                                                cipherText += (char)(ch - (26 - shift));
+                                        } else {
+                                                cipherText += c;
+                                        }
+                                }
+                                if (Character.isUpperCase(ch)) {
+                                        char c = (char)(ch + shift);
+                                        if (c > 'Z') {
+                                                cipherText += (char)(ch - (26 - shift));
+                                        } else {
+                                                cipherText += c;
+                                        }
+                                }
+                        } else {
+                                cipherText += ch;
+                        }
+                }
+
+                return cipherText;
         }
 
-        return cipherText;
+        public static String decrypt(String plainText, int shift) {
+                if (shift > 26) {
+                        shift = shift % 26;
+                } else if (shift < 0) {
+                        shift = (shift % 26) + 26;
+                }
+                String cipherText = "";
+                int length = plainText.length();
+                for (int i = 0; i < length; i++) {
+                        char ch = plainText.charAt(i);
+                        if (Character.isLetter(ch)) {
+                                if (Character.isLowerCase(ch)) {
+                                        char c = (char)(ch - shift);
+                                        if (c < 'a') {
+                                                cipherText += (char)(ch + (26 - shift));
+                                        } else {
+                                                cipherText += c;
+                                        }
+                                }
+                                if (Character.isUpperCase(ch)) {
+                                        char c = (char)(ch - shift);
+                                        if (c < 'A') {
+                                                cipherText += (char)(ch + (26 - shift));
+                                        } else {
+                                                cipherText += c;
+                                        }
+                                }
+                        } else {
+                                cipherText += ch;
+                        }
+                }
+
+                return cipherText;
         }
 
-public static String decrypt(String plainText, int shift){
-        if (shift > 26){
-        shift = shift%26;
-        }
-        else if(shift<0){
-        shift = (shift%26) + 26;
-        }
-        String cipherText = "";
-        int length = plainText.length();
-        for (int i = 0; i <length; i++){
-        char ch = plainText.charAt(i);
-        if (Character.isLetter(ch)){
-        if (Character.isLowerCase(ch)){
-        char c = (char)(ch - shift);
-        if (c < 'a') {
-        cipherText += (char)(ch + (26 - shift));
-        }
-        else {
-        cipherText +=c;
-        }
-        }
-        if (Character.isUpperCase(ch)){
-        char c = (char)(ch - shift);
-        if (c < 'A') {
-        cipherText += (char)(ch + (26 - shift));
-        }
-        else {
-        cipherText +=c;
-        }
-        }
-        }
-        else{
-        cipherText += ch;
-        }
-        }
-
-        return cipherText;
-        }
-
-        }
+}
